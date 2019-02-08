7a73e903891d0618acf5e5dbcc55da4c776790ae
Sebastian Ene <sene@fitbit.com>
2019-02-08 10:13:01 +0200

    Adedd CMSIS functions and enabled SysTick interrupt

--- arch/arm/nrf52840/startup.c
+++ arch/arm/nrf52840/startup.c
@@ -1,5 +1,6 @@
 #include <stdint.h>
 #include <s_heap.h>
+#include <core_cm4.h>
 
 #define RAM_BASE  (void *)(0x20000000)
 #define STACK_TOP  (void *)(RAM_BASE + 0x5000)
@@ -7,6 +8,16 @@
 #define HEAP_START (void *)(RAM_BASE + 0x5004)
 #define HEAP_END   (void *)(RAM_BASE + 0x19000)
 #define HEAP_BLOCK_SIZE (16)
+
+#define  XTAL            (50000000UL)     /* Oscillator frequency */
+
+#define  SYSTEM_CLOCK    (XTAL / 2U)
+
+/*----------------------------------------------------------------------------
+  System Core Clock Variable
+ *----------------------------------------------------------------------------*/
+uint32_t SystemCoreClock = SYSTEM_CLOCK;  /* System Core Clock Frequency */
+
 
 extern unsigned long _stext;
 extern unsigned long _sbss;
@@ -26,6 +37,11 @@ int main();
 
 void SysTick_Handler(void);
 
+void miau(void)
+{
+  while(1) {;;}
+}
+
 __attribute__((section(".isr_vector")))
 void (*vectors[])(void) = {
         STACK_TOP,
@@ -43,7 +59,7 @@ void (*vectors[])(void) = {
         dummy_fn,
         dummy_fn,
         dummy_fn,
-        SysTick_Handler,
+        miau,
 
         /* External interrupts */
 };
@@ -76,6 +92,10 @@ void c_startup(void)
          HEAP_START,
          HEAP_END,
          HEAP_BLOCK_SIZE);
+
+  /* Configure Sys Tick */
+
+  SysTick_Config(SystemCoreClock / 1000);
 
   /* Start the main program */
 
