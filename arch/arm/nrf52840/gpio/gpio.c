670e74edad5bc7bd8dc088706a2a53666e3b2f6e
Sebastian Ene <sebastian.ene07@gmail.com>
2019-02-14 09:00:23 +0200

    Extended gpio functions to accept input configuration

--- arch/arm/nrf52840/gpio/gpio.c
+++ arch/arm/nrf52840/gpio/gpio.c
@@ -3,8 +3,11 @@
 #include <gpio.h>
 
 #define GPIO_BASE   0x50000000
+
 #define GPIO_OUTSET (*((uint32_t*) (GPIO_BASE + 0x508)))
 #define GPIO_OUTCLR (*((uint32_t*) (GPIO_BASE + 0x50C)))
+#define GPIO_IN     (*((uint32_t*) (GPIO_BASE + 0x510)))
+#define GPIO_DIR    (*((uint32_t*) (GPIO_BASE + 0x514)))
 #define GPIO_PIN_CNF(pin) (*((uint32_t*) (GPIO_BASE + 0x700 + pin * 4)))
 
 
@@ -13,9 +16,15 @@ void gpio_init(void)
 
 }
 
-void gpio_configure(int pin)
+void gpio_configure(int pin, gpio_direction_t cfg)
 {
-  GPIO_PIN_CNF(pin) = 1;
+  GPIO_PIN_CNF(pin) = cfg;
+  GPIO_DIR |= (cfg << pin);
+}
+
+int gpio_read(int pin)
+{
+  return ((GPIO_IN & (1 << pin)) >> pin);
 }
 
 void gpio_toogle(int enable, int pin)
