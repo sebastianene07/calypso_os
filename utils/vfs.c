428529d6b43c926e075a53b4af0231522da3d71d
Sebastian Ene <sene@fitbit.com>
2019-03-19 09:08:38 +0200

    Add errno.h header file and set up basic VFS root paths

--- utils/vfs.c
+++ utils/vfs.c
@@ -1,5 +1,6 @@
 #include <vfs.h>
 #include <semaphore.h>
+#include <errno.h>
 
 /* Virtual file system tree */
 
@@ -26,10 +27,34 @@ static struct vfs_ops_s *vfs_get_ops(enum vfs_node_type node_type)
  *  and creates the nodes: /, /dev, /mnt, /var, /bin
  *
  */
-int vfs_init(void)
+int vfs_init(const char *node_name[], size_t num_nodes)
 {
   sem_init(&g_vfs_sema, 0, 1);
 
+  INIT_LIST_HEAD(&g_root_vfs.parent_node);
+  INIT_LIST_HEAD(&g_root_vfs.child_node);
+
   g_root_vfs.name       = "/";
   g_root_vfs.node_type  = VFS_TYPE_DIR;
+
+  /* Create the VFS child nodes */
+
+  struct vfs_node_s *new_node;
+
+  new_node = calloc(sizeof(struct vfs_node_s), num_nodes);
+  if (new_node == NULL)
+  {
+    return -ENOMEM;
+  }
+
+  for (int i = 0; i < num_nodes; ++i)
+  {
+    INIT_LIST_HEAD(&new_node->child_node);
+    new_node->name      = node_name[i];
+    new_node->node_type = VFS_TYPE_DIR;
+
+    list_add(&new_node->parent_node, &g_root_vfs.child_node);
+  }
+
+  return OK;
 }
