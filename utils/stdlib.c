c571a46e3f50f3aa4da6ddb5f92dde0b9516145c
Sebastian Ene <sebastian.ene07@gmail.com>
2019-07-29 08:23:03 +0300

    Implemented realloc in submodule, updated head to track submodule changes

--- utils/stdlib.c
+++ utils/stdlib.c
@@ -26,7 +26,7 @@ void *calloc(size_t nmemb, size_t size)
 
 void *realloc(void *ptr, size_t size)
 {
-//  return s_realloc(ptr, size);
+  return s_realloc(ptr, size, &g_my_heap);
 }
 
 void *reallocarray(void *ptr, size_t nmemb, size_t size)
