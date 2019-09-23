e8c9dc9b8ea83b10135734a2aeff11b83478634b
Sebastian Ene <sebastian.ene07@gmail.com>
2019-09-23 08:31:51 +0300

    Added GNU copyright header to the spi source code driver

--- arch/arm/nrf52840/spi/spi.c
+++ arch/arm/nrf52840/spi/spi.c
@@ -1,6 +1,28 @@
+/*
+ * This file is part of the CatOS distribution https://github.com/catos.
+ *
+ * Copyright (c) 2019 Sebastian Ene.
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, version 3.
+ *
+ * This program is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program. If not, see <http://www.gnu.org/licenses/>.
+ */
+
 #include <spi.h>
 #include <board.h>
 #include <stdlib.h>
+
+/****************************************************************************
+ * Macros Defintions
+ ****************************************************************************/
 
 #define SPI_M0_BASE            (0x40003000)
 
@@ -46,7 +68,6 @@
 /****************************************************************************
  * Private Functions
  ****************************************************************************/
-
 
 /*
  * spi_configure_pins - pin configuration function
