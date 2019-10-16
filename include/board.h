dd2a7fdece803fdd43c181477d0e39630308478e
Sebastian Ene <sebastian.ene07@gmail.com>
2019-10-16 08:56:01 +0300

    Fixed compile time error for nrf52840 board

--- include/board.h
+++ include/board.h
@@ -1,19 +1,20 @@
-#ifndef __BOARD_H
-#define __BOARD_H
+#ifndef __CORE_CM4
+#define __CORE_CM4
 
 #include <stdint.h>
 #include <board_cfg.h>
-#include <versatilepb.h>
+#include <nrf52840.h>
 
 /****************************************************************************
  * Pre-processor Definitions
  ****************************************************************************/
 
-#define RAM_BASE              (void *)(0x10000)
+#define RAM_BASE              (void *)(0x20000000)
 #define STACK_TOP             (void *)(0xDEADBEEF)
 
 #define HEAP_BLOCK_SIZE       (16)
 
+
 void board_init(void);
 
-#endif /* __BOARD_H */
+#endif /* __CORE_CM4 */
