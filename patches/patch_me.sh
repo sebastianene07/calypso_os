9d96bf308d6735e2bbc1536b21937083368514cb
Sebastian Ene <sebastian.ene07@gmail.com>
2019-11-20 10:51:38 +0200

    Patch FatFS library to fix invalid return code on low layer driver function access
    Signed-off-by: Sebastian Ene <sebastian.ene07@gmail.com>

--- patches/patch_me.sh
+++ patches/patch_me.sh
@@ -1,3 +1,6 @@
 #!/bin/sh
 
-cd ./lib/fatfs && git apply --reject --whitespace=nowarn --ignore-space-change --ignore-whitespace  ../patches/patch_fix_compilation_warnings 2> /dev/null
+TOPDIR=$(pwd)
+cd lib/fatfs && git apply --reject --whitespace=nowarn --ignore-space-change --ignore-whitespace  ../patches/patch_fix_compilation_warnings 2> /dev/null
+git am $TOPDIR/patches/0001-Bugfix-Return-correct-status-codes-from-low-level-ac.patch 
+git am $TOPDIR/patches/0001-Use-MMC-as-disk-number-0-in-this-system.patch
