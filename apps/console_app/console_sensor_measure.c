2a99d762591f16a32f97e7af83063f70b8a94a35
Sebastian Ene <sebastian.ene07@gmail.com>
2019-12-19 17:24:40 +0200

    Add simple console tool to access sensor data
    Signed-off-by: Sebastian Ene <sebastian.ene07@gmail.com>

---
+++ apps/console_app/console_sensor_measure.c
@@ -0,0 +1,34 @@
+#include <board.h>
+#include <console_main.h>
+
+#include <stdio.h>
+#include <string.h>
+#include <unistd.h>
+#include <stdbool.h>
+#include <errno.h>
+#include <string.h>
+
+#include <source/ff.h>
+#include <vfs.h>
+#include <stdio.h>
+
+int console_sensor_measure(int argc, const char *argv[])
+{
+  int sensor_fd = open(CONFIG_SENSOR_BME680_PATH_NAME, 0);
+  if (sensor_fd < 0) {
+    printf("Error %d open\n", sensor_fd);
+    return sensor_fd;
+  }
+
+  uint8_t buffer[80];
+
+  int ret = read(sensor_fd, buffer, sizeof(buffer)); 
+  if (ret < 0) {
+    printf("Error %d get sensor data\n", ret);
+  } else {
+    printf("Received %d bytes from sensor\n", ret);
+  }
+
+  close(sensor_fd);
+  return OK;
+}
