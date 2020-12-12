1f4c8a9992290f169b5788076ea723e334c48b66
Sebastian Ene <sebastian.ene07@gmail.com>
2020-12-12 08:33:17 +0200

    esp32: Update board interface

--- arch/xtensa/lx6/board_initialize.c
+++ arch/xtensa/lx6/board_initialize.c
@@ -1,5 +1,6 @@
 #include <board.h>
 
+#include <errno.h>
 #include <serial.h>
 #include <stdint.h>
 #include <scheduler.h>
@@ -17,6 +18,13 @@ __attribute__((section(".boot_entry"))) void bootloader_entry(void)
   __start();
 }
 
+/* __assert_func - create a dump 
+ *
+ */
+void __assert_func(bool assert_cond)
+{
+} 
+
 /*
  * board_init - initialize the board resources
  *
@@ -28,18 +36,63 @@ void board_init(void)
 }
 
 /*
- * up_initial_task_context - creates the initial state for a task
+ * cpu_inittask - creates the initial state for a task
  *
  */
-int up_initial_task_context(struct tcb_s *task_tcb, int argc, char **argv)
+int cpu_inittask(struct tcb_s *tcb, int argc, char **argv)
 {
-  /* Initial MCU context */
+  return -ENOSYS;
+}
 
-  task_tcb->mcu_context[0] = (void *)argc;
-  task_tcb->mcu_context[1] = (void *)argv;
-  task_tcb->mcu_context[5] = (uint32_t *)sched_default_task_exit_point;
-  task_tcb->mcu_context[6] = task_tcb->entry_point;
-  task_tcb->mcu_context[7] = (uint32_t *)0x1000000;
+/*
+ * cpu_destroytask - creates the initial state for a task
+ *
+ */
+void cpu_destroytask(tcb_t *tcb)
+{
+}
 
+/*
+ * cpu_savecontext - save the task context
+ *
+ */
+int cpu_savecontext(void *mcu_context)
+{
   return 0;
+}
+
+/*
+ * cpu_restorecontext - save the task context
+ *
+ */
+void cpu_restorecontext(void *mcu_context)
+{
+}
+
+/*
+ * cpu_disableint - Disable all the interrupts and return the primask register.
+ *
+ */
+irq_state_t cpu_disableint(void)
+{
+  return 0;
+}
+
+/*
+ * cpu_enableint - Enable the interrupts if the first bit of the irq_state is 1.
+ *
+ */
+void cpu_enableint(irq_state_t irq_state)
+{
+}
+
+/*
+ * cpu_getirqnum - Return the interrupt number.
+ *
+ * Assumptions: This should be called only from ISR handler.
+ *
+ */
+int cpu_getirqnum(void)
+{
+  return -ENOSYS;
 }
