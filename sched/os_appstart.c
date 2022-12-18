39db0eb01188b79f6a36b986eb35824f58a23e4b
dedsecn00b <dedsecn00b@gmail.com>
2022-12-18 11:17:49 +0000

    Prepare support for SEPARATE_BUNDLES
    During SEPARATE_BUNDLES we cannot jump directly in the apps because they
    are not linked in the same binary as our kernel image.
    
    Signed-off-by: dedsecn00b <dedsecn00b@gmail.com>

--- sched/os_appstart.c
+++ sched/os_appstart.c
@@ -15,8 +15,12 @@ void os_appstart(void)
   /* Kick off the console application */
 
 #ifdef CONFIG_CONSOLE_APP
-  sched_create_task(console_main, CONFIG_CONSOLE_STACK_SIZE, 0, NULL, "Console");
-#endif
+# ifdef CONFIG_SEPARATE_BUNDLES
+    /* TODO bootsrap some initrd code */
+# else
+    sched_create_task(main, CONFIG_CONSOLE_STACK_SIZE, 0, NULL, "Console");
+# endif /* CONFIG_SEPARATE_BUNDLES */
+#endif /* CONFIG_CONSOLE_APP */
 
   /* The scheduler already kicked of the Idle task which is reponsible for
    * manageing the system resources, entering sleep and calling the scheduler
